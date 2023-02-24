5177dc4f2ce1506a957170bd49ac5b3fb6a64177
Cyrus <sajruz@gmail.com>
2023-02-24 11:35:22 +0000

    Damn skippy

--- __init__.py
+++ __init__.py
@@ -1,25 +1,23 @@
 import os
 from flask import Flask
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'octa.sqlite'),
-    )
+def create_app(cfg=None):
+    app = Flask(__name__)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    load_config(app, cfg)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # import all route modules
+    # and register blueprints
 
     return app
+
+def load_config(app, cfg):
+    # Load a default configuration file
+    app.config.from_pyfile('config/default.cfg')
+
+    # If cfg is empty try to load config file from environment variable
+    if cfg is None and 'MYAPP_CFG' in os.environ:
+        cfg = os.environ['MYAPP_CFG']
+
+    if cfg is not None:
+        app.config.from_pyfile(cfg)
